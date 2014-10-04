628bb7e011668012d9f9594b94d5ec82755b0223
Matthew Barlocker <mbarlocker@gmail.com>
2014-10-04 11:16:26 -0600

    delete HTTP api

--- server/app/ai/rattlesnakes/server/controllers/IndexController.scala
+++ server/app/ai/rattlesnakes/server/controllers/IndexController.scala
@@ -5,7 +5,7 @@ import play.api.mvc.{Action, Controller}
 object IndexController extends Controller {
 
   def get = Action {
-    Ok//Redirect(routes.games.GamesController.get())
+    NotImplemented
   }
 
 }
